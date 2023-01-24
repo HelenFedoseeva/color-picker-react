7447054abfd1abb1b0ac7dbe09634f0b82887246
Helen <helenlena52@gmail.com>
2023-01-24 12:15:02 +0200

    add-color-picker-card

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ColorPicker from "../components/ColorPicker/ColorPicker";
+
+const colorPickerOptions = [
+  { label: "red", color: "#F44336" },
+  { label: "green", color: "#4CAF50" },
+  { label: "blue", color: "#2196F3" },
+  { label: "grey", color: "#607D8B" },
+  { label: "indigo", color: "#3F51B5" },
+  { label: "pink", color: "#E91E63" },
+];
+
+const App = () => {
+  <>
+    <ColorPicker options={colorPickerOptions} />
+  </>;
+};
+
+export default App;
