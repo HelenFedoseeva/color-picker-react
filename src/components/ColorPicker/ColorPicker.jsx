20ed1ab023f246758467f1a9c7119a12953c61a6
Helen <helenlena52@gmail.com>
2023-01-25 11:17:29 +0200

    add-button-click-logic

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -3,31 +3,45 @@ import "./ColorPicker.css";
 
 class ColorPicker extends Component {
   state = {
-    activeOptionIdx: 1,
+    activeOptionIdx: 5,
   }
 
+  setActiveIdx = (index) => {
+    this.setState({activeOptionIdx: index})
+  }
 
-  render() {
-     return (<div className="ColorPicker">
-      <h2 className="ColorPicker__title">The HEX format of the color is:</h2>
-      <div className="colorPicker__container">
-         {this.props.options.map(({ label, color }, index) => {
-           const optionClasses = ["ColorPicker__option"];
+  makeOptionClassName = (index) => {
+    const optionClasses = ["ColorPicker__option"];
 
            if (index === this.state.activeOptionIdx) {
              optionClasses.push("ColorPicker__active")
-           }
+    }
+    
+    return optionClasses.join(' ')
            
+  }
+
+  render() {
+    const {color} = this.props.options[this.state.activeOptionIdx];
+
+
+     return (<div className="ColorPicker">
+       <h2 className="ColorPicker__title">The HEX format of the color is: <span>{ color }</span></h2>
+      <div className="colorPicker__container">
+         {this.props.options.map(({ label, color }, index) => {
+         
           return (
             <button
               key={label}
-              className={optionClasses.join(' ')}
+              className={ this.makeOptionClassName(index) }
               style={{
                 background: color,
               }}
+              onClick={()=> this.setActiveIdx(index)}
             ></button>
           );
-        })}
+         })}
+         
       </div>
     </div>)
    }
