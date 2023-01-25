24c3e292ad9c5b49c92b0a5594b12a4f6c856d28
Helen <helenlena52@gmail.com>
2023-01-25 13:27:23 +0200

    Updates

---
+++ precache-manifest.b14efea3e3c91180d594717a07ad8128.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8689f89e1b87f062711",
+    "url": "/color-picker-react/static/css/main.1ae3e510.chunk.css"
+  },
+  {
+    "revision": "c8689f89e1b87f062711",
+    "url": "/color-picker-react/static/js/main.c8689f89.chunk.js"
+  },
+  {
+    "revision": "fdf175286c6cdd58696b",
+    "url": "/color-picker-react/static/js/1.fdf17528.chunk.js"
+  },
+  {
+    "revision": "a859093cea64473a5a9e",
+    "url": "/color-picker-react/static/js/runtime~main.a859093c.js"
+  },
+  {
+    "revision": "af09d9c8a48fff0b4192d8c541c9724d",
+    "url": "/color-picker-react/index.html"
+  }
+];
